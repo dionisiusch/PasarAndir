db02b1207492b6203800fbe38309852f59e9a8ca
Shandy Hadinata <hadinata.shandy@gmail.com>
2020-08-14 20:43:59 +0700

    area CRUD

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
+    /** @var AreaService */
+    private $areaService;
+
+    /** @var FloorService */
+    private $floorService;
+
+    public function __construct()
+    {
+        $this->areaService = app(AreaService::class);
+        $this->floorService = app(FloorService::class);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +26,10 @@ class AreaController extends Controller
      */
     public function index()
     {
-        //
+        $areas = $this->areaService->showAllAreas();
+        $floors = $this->floorService->showAllFloors();
+
+        // return view('areas.index', compact('areas', 'floors')); 
     }
 
     /**
@@ -35,7 +50,15 @@ class AreaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'floor_id'=>'required',
+            'name'=>'required',
+            'price'=>'required'
+        ]);
+
+        $response = $this->areaService->createArea($request);
+
+        // return redirect('/areas')->with('success', 'Area has been added.');
     }
 
     /**
@@ -44,9 +67,12 @@ class AreaController extends Controller
      * @param  \App\Model\Area  $area
      * @return \Illuminate\Http\Response
      */
-    public function show(Area $area)
+    public function show($id)
     {
-        //
+        $area = $this->areaService->getAreaById($id);
+        $floor = $this->floorService->getFloorById($id);
+
+        // return view('areas.show', compact('area', 'floor')); 
     }
 
     /**
@@ -67,9 +93,17 @@ class AreaController extends Controller
      * @param  \App\Model\Area  $area
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Area $area)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'floor_id'=>'required',
+            'name'=>'required',
+            'price'=>'required'
+        ]);
+
+        $response = $this->areaService->updateAreaById($request, $id);
+
+        // return redirect('/areas')->with('success', 'Area has been updated.');
     }
 
     /**
@@ -80,6 +114,8 @@ class AreaController extends Controller
      */
     public function destroy(Area $area)
     {
-        //
+        $response = $this->areaService->deleteAreaById($id);
+
+        // return redirect('/areas')->with('success', 'Area has been deleted');
     }
 }
---
+++ app/Http/Services/AreaService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Model\Area;
+
+class AreaService
+{
+    public function showAllAreas()
+    {
+        $areas = Area::all();
+
+        return $areas;
+    }
+
+    public function createArea($data)
+    {
+        $area = new Area([
+            'floor_id' => $data->get('floor_id'),
+            'name' => $data->get('name'),
+            'price' => $data->get('price')
+        ]);
+        $area->save();
+
+        return $area;
+    }
+
+    public function getAreaById($id)
+    {
+        $area = Area::find($id);
+        return $area;
+    }
+
+    public function updateAreaById($data, $id)
+    {
+        $area = Area::find($id);
+        $area->floor_id = $data->get('floor_id');
+        $area->name = $data->get('name');
+        $area->price = $data->get('price');
+        $area->save();
+
+        return $area;
+    }
+
+    public function deleteAreaById($id)
+    {
+        $area = Area::find($id);
+        $area->delete();
+
+        return $area;
+    }
+}
