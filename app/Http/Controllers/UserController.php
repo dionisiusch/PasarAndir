46d1ae4bec89e8fb78c6da62ccc726fd12ce3b5c
Shandy Hadinata <hadinata.shandy@gmail.com>
2020-08-20 23:13:09 +0700

    bug user service

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $stalls = $this->userService->showAllUsers();
+        $users = $this->userService->showAllUsers();
 
         // return view('master.user.userShow');
     }
--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services;
 
 use App\User;
+use DB;
 
 class UserService
 {
