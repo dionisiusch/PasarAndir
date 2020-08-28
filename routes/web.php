ec9c6011b293b6070398e792065252423527abe1
dionisiusch <dionisius.ch@gmail.com>
2020-08-28 23:45:29 +0700

    d

--- routes/web.php
+++ routes/web.php
@@ -122,5 +122,4 @@ Route::get('/floorselect2', 'floorController@select2')->name('master.floor.selec
 Route::get('/areaselect2', 'areaController@select2')->name('master.area.select2');
 Route::get('/userselect2', 'userController@select2')->name('master.user.select2');
 Route::get('/categoryselect2', 'categoryController@select2')->name('master.category.select2');
-
 Route::get('/employerselect2', 'employerController@select2')->name('master.employer.select2');
